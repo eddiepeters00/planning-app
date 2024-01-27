807887976d0eb5ee984f7f1c5b6e40ae6938f8d3
eddiepeters00 <eddie.peters00@outlook.com>
2024-01-27 23:43:30 +0100

    Added some more components and tested them out

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,213 @@
-import { Card, CardDescription, CardTitle } from "./components/ui/card";
+import { Calendar } from "./components/ui/calendar";
+import { useState } from "react";
+import { ColumnDef } from "@tanstack/react-table";
+import { DataTable } from "./components/ui/DataTable/DataTable";
+import { MoreHorizontal, PlusIcon } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "./components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
 function App() {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+
+  const formSchema = z.object({
+    priority: z.number(),
+    title: z.string().min(2).max(50),
+    description: z.string(),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      priority: 1,
+      title: "Title",
+      description: "Description",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  type TodoType = {
+    id: string;
+    priority: 1 | 2 | 3 | 4 | 5;
+    title: string;
+  };
+
+  const todoData: TodoType[] = [
+    {
+      id: "728ed52f",
+      priority: 1,
+      title: "Very important information",
+    },
+    {
+      id: "489e1d42",
+      priority: 1,
+      title: "Shoppinglist",
+    },
+  ];
+
+  const columns: ColumnDef<TodoType>[] = [
+    {
+      accessorKey: "priority",
+      header: "Priority",
+    },
+    {
+      accessorKey: "title",
+      header: "Title",
+    },
+    {
+      id: "actions",
+      header: () => (
+        <Drawer>
+          <DrawerTrigger asChild>
+            <Button variant="outline">
+              <PlusIcon />
+            </Button>
+          </DrawerTrigger>
+          <DrawerContent>
+            <DrawerHeader className="text-left">
+              <DrawerTitle>Add new item</DrawerTitle>
+              <DrawerDescription>
+                Add the information you want your item to include. Click save
+                when you're done.
+              </DrawerDescription>
+            </DrawerHeader>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="priority"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Enter priority</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Priority" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Enter a title</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Title" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Enter a description</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Description" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
+            ;
+            <DrawerFooter className="pt-2">
+              <DrawerClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      ),
+      cell: ({ row }) => {
+        const payment = row.original;
+
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => navigator.clipboard.writeText(payment.id)}
+              >
+                open
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => console.log("Delete")}>
+                Delete
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ];
+
   return (
     <>
-      <Card className="w-fit p-2">
-        <CardTitle>Test</CardTitle>
-        <CardDescription>This is a description</CardDescription>
-        <CardDescription>This is a description</CardDescription>{" "}
-        <CardDescription>This is a description</CardDescription>{" "}
-        <CardDescription>This is a description</CardDescription>{" "}
-        <CardDescription>This is a description</CardDescription>{" "}
-        <CardDescription>This is a description</CardDescription>{" "}
-        <CardDescription>This is a description</CardDescription>{" "}
-        <CardDescription>This is a description</CardDescription>{" "}
-        <CardDescription>This is a description</CardDescription>
-      </Card>
+      <main className="grid grid-flow-col">
+        <section>
+          <DataTable columns={columns} data={todoData} />
+        </section>
+
+        <section>
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            className="rounded-md border w-fit"
+          />
+        </section>
+      </main>
     </>
   );
 }
